2a4f42e143b5e12f64a58b5dccbfcfb642336b3b
Mark Nadig <MarkNadig@Deltek.com>
2018-03-13 13:10:56 -0600

    another

--- PoorMark/Controllers/SQLController.cs
+++ PoorMark/Controllers/SQLController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using PoorMansTSqlFormatterRedux.;
+using PoorMansTSqlFormatterRedux;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
